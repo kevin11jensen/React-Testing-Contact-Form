f340fc05865b7f870b2b27334e5ee17cb91a3fef
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-02-21 15:38:28 -0800

    first test run for first name input

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -2,3 +2,10 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import ContactForm from "./ContactForm";
 
+test('renders labels?', () => {
+    const { getByLabelText } = render(>ContactForm />);
+
+    const firstNameLabel = getByLabelText(/first name/i)
+
+    expect(firstNameLabel).toBeInTheDocument();
+});
